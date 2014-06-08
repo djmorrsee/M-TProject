e45f1200f919abc51107b4709625399569b879c5
Daniel Morrissey <djmorrsee@gmail.com>
2014-06-08 14:34:12 -0600

    Merging our Echos

--- server.py
+++ server.py
@@ -9,12 +9,12 @@ def home():
     return render_template('base.html', temp = temp)
 
 @app.route('/dj_echo/', methods=['POST'])
-def echo():
+def dj_echo():
 	if request.method == 'POST':
 		print(request.data)
 	return ''
 
-@app.route('/echo/', methods = ['POST', 'GET'])
+@app.route('/echo/', methods = ['POST'])
 def echo():
     global temp
     if request.method == 'POST':
