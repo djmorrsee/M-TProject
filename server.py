b8314f627cfdbca727fa712480b4a69681fb6cc6
Andrey Shprengel <andrey.shprengel@colorado.edu>
2014-06-08 11:43:08 -0600

    Added a base to display most current temp

--- server.py
+++ server.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template, request
+import json
 
 app = Flask(__name__)
-
+name = "None"
+temp = "None"
 @app.route('/')
 def home():
-	return render_template('base.html')
+    return render_template('base.html', temp = temp)
 
+@app.route('/echo/', methods = ['POST', 'GET'])
+def echo():
+    global temp
+    if request.method == 'POST':
+        print temp
+        temp= request.json['temp']
+        return "succes"
+   
+       
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
