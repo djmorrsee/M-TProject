fb555bd048874d8a4fce16b6541733dd8caa83f4
Andrey Shprengel <andrey.shprengel@colorado.edu>
2014-07-02 09:54:14 -0600

    Added copyright notice to files

--- bin/data/data.py
+++ bin/data/data.py
@@ -1,3 +1,7 @@
+#Copyright (C) 2014 Andrey Shprengel <andrey.shprengel@colorado.edu> Daniel Morrissey <daniel.morrissey@colorado.edu>
+#This work is free. You can redistribute it and/or modify it under the
+#terms of the Do What The Fuck You Want To Public License, Version 2,
+#as published by Sam Hocevar. See the COPYING file for more details.
 """ Reading Data
 
 This file contains methods to ensure data format conformation
--- bin/util/test_status_codes.py
+++ bin/util/test_status_codes.py
@@ -1,5 +1,8 @@
 #pylint: skip-file
-
+#Copyright (C) 2014 Andrey Shprengel <andrey.shprengel@colorado.edu> Daniel Morrissey <daniel.morrissey@colorado.edu>
+#This work is free. You can redistribute it and/or modify it under the
+#terms of the Do What The Fuck You Want To Public License, Version 2,
+#as published by Sam Hocevar. See the COPYING file for more details.
 import unittest
 import status_codes
 
